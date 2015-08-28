ea66e91eb68ff6600932d1d785ed25d8816be073
Mohsin <muhammad.mohsin.991@gmail.com>
2015-08-28 18:23:13 +0500

    Initial Commit

---
+++ MyCompiler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyCompiler
+//
+//  Created by Mohsin on 28/08/2015.
+//  Copyright (c) 2015 Mohsin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
